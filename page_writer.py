07434a6f331baa1de0a364bab9d055d97ca164e4
Martin Chorley <me@martinjc.com>
2016-08-24 15:49:02 +0100

    updated links and info

--- page_writer.py
+++ page_writer.py
@@ -8,16 +8,16 @@ modules = {
     "CMT108": "",
     "CMT111": "https://cardiff.cloud.panopto.eu/Panopto/Pages/Embed.aspx?id=ebbb89d0-dfac-407c-84a1-22daec0dc908",
     "CMT112": "https://cardiff.cloud.panopto.eu/Panopto/Pages/Embed.aspx?id=3446edae-d762-49c4-8302-a8cf74a1f315",
-    "CMT202": "",
+    "CMT202": "https://cardiff.cloud.panopto.eu/Panopto/Pages/Embed.aspx?id=360b3717-0492-4810-a8fe-03c289c1f9fa",
     "CMT205": "https://cardiff.cloud.panopto.eu/Panopto/Pages/Embed.aspx?id=ba3a49c4-7fd3-4ea8-985e-c75fb0ab72fb",
     "CMT206": "https://cardiff.cloud.panopto.eu/Panopto/Pages/Embed.aspx?id=c033da0c-012e-43e1-856f-b9da64ef6f01",
-    "CMT207": "",
-    "CMT209": "",
+    "CMT207": "https://cardiff.cloud.panopto.eu/Panopto/Pages/Embed.aspx?id=a909e025-b20d-4412-835d-587315566301",
+    "CMT209": "https://cardiff.cloud.panopto.eu/Panopto/Pages/Embed.aspx?id=9d765d94-dca7-444a-80d5-81a6ecf10d34",
     "CMT212": "https://cardiff.cloud.panopto.eu/Panopto/Pages/Embed.aspx?id=4b02ae8b-16da-4ffa-a50f-31e40682f85d",
     "CMT213": "https://cardiff.cloud.panopto.eu/Panopto/Pages/Embed.aspx?id=4d8e28b8-127a-4905-80d8-2f54456df9b3",
     "CMT301": "https://cardiff.cloud.panopto.eu/Panopto/Pages/Embed.aspx?id=b7683eda-c278-4fb5-b907-51886b9bf9c8",
     "CMT302": "",
-    "CMT303": "",
+    "CMT303": "https://cardiff.cloud.panopto.eu/Panopto/Pages/Embed.aspx?id=33a0ef4d-43e6-436e-89dc-fae7547cf5a5",
     "CMT304": "",
     "CMT306": "https://cardiff.cloud.panopto.eu/Panopto/Pages/Embed.aspx?id=04f99bf3-4d14-4f18-ae3a-e1d69aae9cc7"
 }
@@ -43,13 +43,12 @@ pfmscdj_opt = ['CMT212', 'CMT111', 'CMT206']
 import requests
 
 """
-<div id="CMT302">
-    <h2>Optional Modules</h2>
-    <h3>CMT302 - E-commerce &amp; Innovation</h3>
-    <a href="http://handbooks.data.cardiff.ac.uk/module/CMT302.html">Module Handbook</a>
-    <img src="img/coming_soon.png" alt="Video Coming Soon">
-    <a href="top">Back to top</a>
-</div>
+        <div id="CMT304">
+        	<h3>CMT304 - Programming Paradigms</h3>
+        	<a href="http://handbooks.data.cardiff.ac.uk/module/CMT304.html">Module Handbook</a>
+        	<img src="img/coming_soon.png" alt="Video Coming Soon">
+        	<a href="#top">Back to top</a>
+        </div>
 """
 
 
@@ -60,9 +59,17 @@ def write_module_div(outfile, module, link):
 
     print(module_data['module']['moduleName'])
 
+    ml_data = module_data['occurrences']['staff']['moduleLeader']
+
+    semester = module_data['occurrences']['semester']['type']
+
+    module_leader = "%s %s %s" % (ml_data['title'], ml_data['firstName'], ml_data['surname'])
+
     outfile.write('<div id="%s">\n' % (module))
     outfile.write('\t<h3>%s - %s</h3>\n' % (module, module_data['module']['moduleName']))
-    outfile.write('\t<a href="%s">Module Handbook</a>\n' % ('%s.html' % handbook_data))
+    outfile.write('\t<p><strong>Module Leader:</strong> %s</p>\n' % (module_leader))
+    outfile.write('\t<p><strong>Semester: </strong>%s</p>\n' % (semester))
+    outfile.write('\t<p>For module information, please see the <a href="%s">Module Handbook</a> and watch the video below</p>\n' % ('%s.html' % handbook_data))
 
     if link:
         outfile.write('\t<iframe src="%s" height="405" style="padding: 0px; border: 1px solid #464646;" frameborder="0"></iframe>\n' % (link))
